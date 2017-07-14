4ebebaffbb7391bc1db183b9cfbd39b0a0536034
Mac <nmacawile@gmail.com>
2017-07-14 14:20:47 +0000

    Implement post deletion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :signed_in_user, only: :destroy
+  before_action :correct_user, only: :destroy
   def index
     @posts = Post.paginate(page: params[:page], per_page: 10)
   end
@@ -8,4 +10,16 @@ class PostsController < ApplicationController
     flash[:info] = "Post has been deleted."
     redirect_to current_user
   end
+  
+  private
+  
+    def signed_in_user
+      redirect_to signin_path unless signed_in?
+    end
+    
+    def correct_user
+      user = Post.find(params[:id]).user
+      flash[:warning] = "That post is not yours"
+      redirect_to root_path unless current_user?(user)
+    end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,5 +5,34 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   #  get posts_index_url
   #  assert_response :success
   #end
+  
+  def setup
+    @post = posts(:one)
+    @poster = @post.user
+    @not_poster = users(:two)
+  end
+  
+  test "should redirect delete when no user is signed in" do
+    assert_no_difference "Post.count" do
+      delete post_path(@post)
+    end
+    assert_redirected_to signin_path
+  end
+  
+  test "should redirect delete when post does not belong to signed in user" do
+    sign_in_as @not_poster
+    assert_no_difference "Post.count" do
+      delete post_path(@post)
+    end
+    assert_redirected_to root_path
+  end
+  
+  test "should delete when a signed in user is deleting his own post" do
+    sign_in_as @poster
+    assert_difference "Post.count", -1 do
+      delete post_path(@post)
+    end
+    assert_redirected_to @poster
+  end
 
 end
